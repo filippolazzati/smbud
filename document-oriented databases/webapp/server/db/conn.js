c45ecd9c1a8123d3b0fd79b29efad3af3fca2a19
Christian Grasso <christian@grasso.io>
2021-12-08 19:04:59 +0100

    Add missing dependency, configurable database name, use HashRouter

--- document-oriented databases/webapp/server/db/conn.js
+++ document-oriented databases/webapp/server/db/conn.js
@@ -13,7 +13,7 @@ module.exports = {
             // Verify we got a good "db" object
             if (db)
             {
-                _db = db.db("certificates_db");
+                _db = db.db(process.env.DATABASE_NAME || "certificates_db");
                 console.log("Successfully connected to MongoDB."); 
             }
             return callback(err);
